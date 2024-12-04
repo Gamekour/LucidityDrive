0a1907963fc5462eb35f5dfb47fb5ddfb865bfc5
Gamekour <Theepicgameexplorer@gmail.com>
2024-12-04 12:31:25 -0500

    fixed event box collision exits
    - event boxes will now fire OnCollisionExit if it hasn't been called after 2 frames since the last OnCollisionStay (this is to fix cases where an object becomes null and doesn't generate an OnCollisionExit)

--- Runtime/Scripts/Environment/EventBox.cs
+++ Runtime/Scripts/Environment/EventBox.cs
@@ -6,6 +6,9 @@ public class EventBox : MonoBehaviour, IGrabTrigger
     public Collider colliderRef;
     [SerializeField] bool doCollisionStayEvents = false;
     private bool initialized = false;
+    private bool justCollided = false;
+    private int framesSinceCollision = 0;
+    private Collision lastCollision = null;
 
     [Header("Events")]
     public UnityEvent<Collider> onTriggered;
@@ -37,20 +40,40 @@ public class EventBox : MonoBehaviour, IGrabTrigger
     {
         if (!initialized) return;
         onCollisionEnter.Invoke(collision);
+        lastCollision = collision;
     }
 
     private void OnCollisionExit(Collision collision)
     {
         if (!initialized) return;
         onCollisionExit.Invoke(collision);
+        framesSinceCollision = 0;
+        justCollided = false;
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if (!initialized) return;
         if (doCollisionStayEvents)
+        {
             onCollisionStay.Invoke(collision);
+            framesSinceCollision = 0;
+            lastCollision = collision;
+            justCollided = true;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        framesSinceCollision++;
+        if (framesSinceCollision > 1 && justCollided)
+        {
+            onCollisionExit.Invoke(lastCollision);
+            justCollided = false;
+        }
+    }
+
+
 
     public void GrabEvent()
     {
