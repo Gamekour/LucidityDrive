16b2d98ebcee918f50da397bbbc3cb7f5225c0ae
Gamekour <49810363+Gamekour@users.noreply.github.com>
2025-06-17 19:59:34 -0400

    b

--- Runtime/Scripts/Player/LucidLegs.cs
+++ Runtime/Scripts/Player/LucidLegs.cs
@@ -143,7 +143,7 @@ public class LucidLegs : MonoBehaviour
     private void ConfigurePhysModel(LucidVismodel visModel)
     {
         physBody.size = Vector3.Scale(visModel.bodyCollider.size, visModel.bodyCollider.transform.lossyScale);
-        physBody.center = Vector3.Scale(visModel.bodyCollider.center, visModel.bodyCollider.transform.lossyScale);
+        //physBody.center = Vector3.Scale(visModel.bodyCollider.center, visModel.bodyCollider.transform.lossyScale);
         physHead.radius = visModel.headCollider.radius * visModel.headCollider.transform.lossyScale.y;
         //physHead.center = visModel.headCollider.center * visModel.headCollider.transform.lossyScale.y;
         ConfigurableJoint hcj = physHead.GetComponent<ConfigurableJoint>();
