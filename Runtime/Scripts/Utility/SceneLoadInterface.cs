dbc086fc727ebc9196acef8342b412db7228b820
Gamekour <Theepicgameexplorer@gmail.com>
2025-09-19 11:26:51 -0400

    Update SceneLoadInterface.cs

--- Runtime/Scripts/Utility/SceneLoadInterface.cs
+++ Runtime/Scripts/Utility/SceneLoadInterface.cs
@@ -5,18 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadInterface : MonoBehaviour
 {
-    private int currentIndex = 0;
-
-    private void OnLevelWasLoaded(int level) => currentIndex = level;
 
     public void LoadScene(string name) => SceneManager.LoadScene(name);
 
     public void LoadScene(int buildIndex) => SceneManager.LoadScene(buildIndex);
 
-    public void LoadNextScene() => SceneManager.LoadScene(currentIndex + 1);
+    public void LoadNextScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
 
-    public void LoadPrevScene() => SceneManager.LoadScene(currentIndex - 1);
+    public void LoadPrevScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 
-    public void ReloadScene() => SceneManager.LoadScene(currentIndex);
+    public void ReloadScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
 }
